a47a26b9b3e75000ae1fcdaa45f6f7ee40e1ec49
Bing <bingturong@yandex.com>
2020-12-19 01:21:07 +0200

    Clean up and sprite addition

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 var gameScene = new Phaser.Scene('Game');
 var config = {
-	type: Phaser.AUTO,  
-	width: 800, 
-	height: 600, 
-	scene: gameScene 
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+        }
+    },
 };
 var game = new Phaser.Game(config);
+gameScene.preload = function () {
+    this.load.image('tbackground', 'assets/tbackground.png');
+
+};
+
+gameScene.create = function () {
+    this.add.sprite(0, 0, 'tbackground');
+    bg.setOrigin(0, 0);
+
+}
