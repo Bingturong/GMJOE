2cdc8669c5961e091410a366d770fee2a6de006b
Bing <bingturong@yandex.com>
2020-12-19 01:32:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-var gameScene = new Phaser.Scene('Game');
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-        }
-    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
+
 var game = new Phaser.Game(config);
-gameScene.preload = function () {
+
+function preload() {
     this.load.image('tBackground', 'assets/tBackground.png');
+    this.load.multiatlas('Demi.Walk.Anubis.R', 'assets/animatedsprites/Demi.Walk.Anubis.R/Demi.Walk.Anubis.R.json', 'Demi.Walk.Anubis.R');
+}
 
-};
+function create() {
+    this.add.image(0, 0, 'tBackground').setOrigin(0, 0)
+}
 
-gameScene.create = function () {
-    this.add.sprite(0, 0, 'tBackground');
-    bg.setOrigin(0, 0);
-
+function update() {
 }
