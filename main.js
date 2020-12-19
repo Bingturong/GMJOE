5712aa7a795a653a5a9a6ee4e01e3f0c4a8956c6
Bing <bingturong@yandex.com>
2020-12-19 04:07:08 +0200

    afq

--- main.js
+++ main.js
@@ -17,7 +17,8 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('tBackground', 'assets/GMJoEsplash.png');
+    this.load.image('tBackground', 'assets/tBackground.png');
+    this.load.image('mj', 'assets/mummyjackal.png');
     this.load.image('tiles', 'assets/tilesets/tset.png');
     this.load.tilemapTiledJSON('map', 'assets/tilemaps/t.json');
     this.load.multiatlas('Demi.Walk.Anubis.R', 'Assets/animatedsprites/Demi.Walk.Anubis.R/Demi.Walk.Anubis.R.json', 'Demi.Walk.Anubis.R');
@@ -25,6 +26,14 @@ function preload() {
 
 function create() {
     this.add.image(0, 0, 'tBackground').setOrigin(0, 0)
+    this.add.image(400, 300, 'mj')
+    anu = this.add.sprite(0, 400, 'Demi.Walk.Anubis.R', '0001.png');
+    anu.setScale(0.2, 0.2);
+    var frameNames = this.anims.generateFrameNames('cityscene', {
+        start: 1, end: 21, zeroPad: 4,
+        prefix: 'capguy/walk/', suffix: '.png'
+    });
+
     const map = this.make.tilemap({ key: 'map' });
     const ts = map.addTilesetImage('t', 'tiles');
     const platforms = map.createStaticLayer('Walls', ts, 0, 200)
