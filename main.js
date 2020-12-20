c74a249a9aa6d2596e9a44dd8be5cb3de5acd9f8
Bing <bingturong@yandex.com>
2020-12-20 19:10:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 ﻿var config = {
     type: Phaser.AUTO,
-    scale: {
-        parent: 'game',
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 800,
-        height: 600,
-    },
+    width: 800,
+    height: 600,
     physics: {
         default: 'arcade',
         arcade: {
