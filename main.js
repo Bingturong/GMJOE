4ed75735240b4df93e65804cfe0a097b05172945
Bing <bingturong@yandex.com>
2020-12-20 19:56:47 +0200

    wq

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 ﻿var config = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
+    width: 800,
+    height: 600,
     physics: {
         default: 'arcade',
         arcade: {
@@ -19,17 +19,18 @@ var game = new Phaser.Game(config);
 function preload() {
     this.load.image('tBackground', 'assets/tBackground.png');
     this.load.image('mj', 'assets/mummyjackal.png');
-    this.load.image('tiles', 'assets/tilesets/tset.png');
-    this.load.tilemapTiledJSON('map', 'assets/tilemaps/t.json');
+    this.load.image('tile70', 'assets/tile70.png');
+    this.load.tilemapTiledJSON('map', 'assets/x4.json');
     this.load.multiatlas('Demi.Walk.Anubis.R', 'Assets/animatedsprites/Demi.Walk.Anubis.R/Demi.Walk.Anubis.R.json', 'Demi.Walk.Anubis.R');
-    game.scale.pageAlignHorizontally = true;
-    game.scale.pageAlignVertically = true;
-    game.scale.refresh();
 }
 
 function create() {
     mj = this.add.image(400, 500, 'mj')
     mj.setScale(0.2, 0.2);
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('tile.70', 'tile70');
+    const platforms = map.createStaticLayer('layer1', tileset, 0, 200);
+    platforms.setCollisionByExclusion(-1, true);
 }
 
 function update() {
